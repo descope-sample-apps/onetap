65e716e1cc37f0fadfbc24bae694bf44ad2e0e57
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2024-08-26 10:54:21 -0700

    Add middleware

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
+
   return (
     <main className="bg-gray-800">
       <OneTapComp />
@@ -14,14 +15,19 @@ export default function Home() {
             Gradients X Animations
           </p> */}
           <Image src='/descope-logo.png' alt='Descope logo' width={200} height={200} />
-          <Card>
-            <h1 className="">Experience Google &quot;One Tap&quot; Login</h1>
-            <h3>Log in like magic! Look at the top-right corner of your screen.</h3>
-            <p>Not seeing the &quot;One Tap&quot; Login popup? You might be on an unsupported browser. Check list <Link target='_blank' href='https://developers.google.com/identity/gsi/web/guides/supported-browsers'>here</Link>.</p>
-          </Card>
+          <Message />
         </div>
       </BackgroundGradientAnimation>
-
     </main>
   );
+}
+
+function Message() {
+  return <Card>
+    <h1 className="">Experience Google &quot;One Tap&quot; Login</h1>
+    <h3>Log in like magic! Look at the top-right corner of your screen.</h3>
+    <p>Not seeing the &quot;One Tap&quot; Login popup? You
+      might be on an unsupported browser. Check list <Link target='_blank' href='https://developers.google.com/identity/gsi/web/guides/supported-browsers'>here</Link>.
+    </p>
+  </Card>
 }
---
+++ middleware.ts
@@ -0,0 +1,20 @@
+// src/middleware.ts
+import { authMiddleware } from '@descope/nextjs-sdk/server'
+
+export default authMiddleware({
+	// The Descope project ID to use for authentication
+	// Defaults to process.env.DESCOPE_PROJECT_ID
+	projectId: process.env.NEXT_PUBLIC_DESCOPE_PROJECT_ID,
+
+	// The URL to redirect to if the user is not authenticated
+	// Defaults to process.env.SIGN_IN_ROUTE or '/sign-in' if not provided
+	// NOTE: In case it contains query parameters that exist in the original URL, they will override the original query parameters. e.g. if the original URL is /page?param1=1&param2=2 and the redirect URL is /sign-in?param1=3, the final redirect URL will be /sign-in?param1=3&param2=2
+
+	// These are the public and private routes in your app. Read more about how to use these below.
+	publicRoutes: ["/"],
+	// If you having privateRoutes and publicRoutes defined at the same time, privateRoutes will be ignored.
+})
+
+export const config = {
+	matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)']
+}
