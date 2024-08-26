de008f1f4ef66901f023d3636ffa78e2cb0de777
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2024-08-26 11:31:27 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -30,16 +30,16 @@ export default function Home() {
 
 function Message( { isAuthenticated } : { isAuthenticated: boolean }) {
   if (isAuthenticated) {
-    return <Card className="backdrop-blur-sm bg-white/20 p-4 space-y-4 max-w-sm mt-8">
+    return <Card className="backdrop-blur-sm bg-white/20 p-4 space-y-3 max-w-sm mt-8">
       <h1 className="text-white text-xl font-bold">You&apos;re logged in!</h1>
       <p className="text-white text-sm font-normal">You&apos;ve just experienced Descope&apos;s &quot;One Tap&quot; Login. Feel free to log out and try it again.</p>
       <LogoutButton />
     </Card>
   }
   return <Card className="backdrop-blur-sm bg-white/20 p-4 space-y-4 max-w-md mt-8">
-    <h1 className="text-gray-100 text-xl font-bold">Experience Google &quot;One Tap&quot; Login</h1>
-    <h3 className="text-gray-300 text-sm font-normal">Log in like magic! Look at the top-right corner of your screen.</h3>
-    <p className="text-gray-300 text-sm font-normal">Not seeing the &quot;One Tap&quot; Login popup? You
+    <h1 className="text-white text-xl font-bold">Experience Google &quot;One Tap&quot; Login</h1>
+    <h3 className="text-gray-200 text-sm font-normal">Log in like magic! Look at the top-right corner of your screen.</h3>
+    <p className="text-gray-200 text-sm font-normal">Not seeing the &quot;One Tap&quot; Login popup? You
       might be on an unsupported browser. Check out the list <Link className="text-gray-300 underline hover:text-gray-100 transition-colors" target='_blank' href='https://developers.google.com/identity/gsi/web/guides/supported-browsers'>here</Link>.
     </p>
   </Card>
