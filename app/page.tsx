94af4c31dd44eecdbb50fb83b9b2138e868bed5c
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2024-08-26 14:43:32 -0700

    New background color

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,13 @@
 'use client'
 
-import LogoutButton from "@/components/features/logout-button";
 import Message from "@/components/features/message";
 import OneTapComp from "@/components/features/one-tap-component";
 import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
-import { Card } from "@/components/ui/card";
-import { useSession } from "@descope/nextjs-sdk/client";
 import Image from "next/image";
-import Link from "next/link";
 
 export default function Home() {
-  const { isAuthenticated, isSessionLoading } = useSession();
-
   return (
-    <main className="bg-gray-800">
+    <main className="bg-[#18283F]">
       <OneTapComp />
       <BackgroundGradientAnimation>
         <div className="absolute z-50 inset-0 flex flex-col items-center justify-center text-white font-bold px-4 text-3xl text-center">
--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -1,15 +1,13 @@
 'use client'
 
 import Message from "@/components/features/message";
-import OneTapComp from "@/components/features/one-tap-component";
 import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
 import Image from "next/image";
 
 export default function Home() {
 
   return (
-    <main className="bg-gray-800">
-      <OneTapComp />
+    <main className="bg-[#18283F]">
       <BackgroundGradientAnimation>
         <div className="absolute z-50 inset-0 flex flex-col items-center justify-center text-white font-bold px-4 text-3xl text-center">
           <Image src='/descope-logo.svg' alt='Descope logo' width={250} height={200} />
