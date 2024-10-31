8da45da11c013775d7e2190194f73a382c935f53
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2024-10-31 15:42:16 -0700

    Update logout-button.tsx

--- components/features/logout-button.tsx
+++ components/features/logout-button.tsx
@@ -2,15 +2,12 @@
 
 import { useDescope } from "@descope/nextjs-sdk/client";
 import { Button } from "../ui/button";
-import { useRouter } from "next/navigation";
 
 export default function LogoutButton() {
 
     const { logout } = useDescope();
-    const router = useRouter();
-
     return <Button variant="secondary" className="active:scale-95 transition-transform" onClick={async () => {
         await logout();
-        router.refresh();
+        window.location.reload()
     }}>Logout</Button>;
 }
